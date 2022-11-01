39471a867fde7d0afd160c38caa2cb78402b74d5
zhangbaoxiang001 <1144653975@qq.com>
2022-11-01 10:24:35 +0800

    二分查找的变种 upper方法

--- src/Binary-Search/BinarySearch.js
+++ src/Binary-Search/BinarySearch.js
@@ -14,4 +14,25 @@ class BinarySearch {
             return this._search(data, mid + 1, r, target)
         return this._search(data, l, mid - 1, target)
     }
+
+    upper(data, target) {
+        let l = 0, r = data.length
+        while (l < r) {
+            let mid = l + (r - l) / 2
+            if (compareTo(data[mid], target) <= 0)
+                l = mid + 1
+            else 
+                r = mid
+        }
+        return l
+    }
+
+    // > target 返回最小值索引
+    // == target 返回最大值索引
+    ceil(data, target) {
+        let u = this.upper(data, target)
+        if (u - 1 >= 0 && compareTo(data[u], target) == 0)
+            return u - 1
+        return u
+    }
 }
